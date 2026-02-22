4ca3492ac0b10ab79d070f2d4ddbac6bce3a87c6
Abdelrahmn ayman <boodyayman936@gmail.com>
2026-02-23 00:33:54 +0200

    feat: Add unit tests for home data source, repository, use case, and view model

---
+++ test/features/home/presentation/cubit/orders_view_model_test.dart
@@ -0,0 +1,96 @@
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:tracking_app/core/bloc/base_state.dart';
+import 'package:tracking_app/core/error_handling/result.dart';
+import 'package:tracking_app/features/home/domain/entities/home_order_entity.dart';
+import 'package:tracking_app/features/home/domain/use_case/get_orders_use_case.dart';
+import 'package:tracking_app/features/home/presentation/cubit/orders_events.dart';
+import 'package:tracking_app/features/home/presentation/cubit/orders_state.dart';
+import 'package:tracking_app/features/home/presentation/cubit/orders_view_model.dart';
+
+import 'orders_view_model_test.mocks.dart';
+
+@GenerateMocks([GetOrdersUseCase])
+void main() {
+  late MockGetOrdersUseCase mockUseCase;
+  late OrdersViewModel viewModel;
+
+  setUp(() {
+    mockUseCase = MockGetOrdersUseCase();
+    viewModel = OrdersViewModel(mockUseCase);
+    provideDummy<Result<List<HomeOrderEntity>>>(Success([]));
+  });
+
+  final tOrders = [
+    HomeOrderEntity(
+      orderId: '1',
+      userName: 'Ahmed',
+      storeName: '',
+      storeAddress: '',
+      storeImage: '',
+      totalPrice: 100.0,
+      userImage: '',
+      status: '',
+      userAddress: '',
+    ),
+  ];
+
+  group('OrdersViewModel Tests', () {
+    blocTest<OrdersViewModel, OrdersState>(
+      'emits [loading, loaded] when GetOrdersIntent is successful',
+      build: () {
+        when(
+          mockUseCase.invoke(any, any),
+        ).thenAnswer((_) async => Success(tOrders));
+        return viewModel;
+      },
+      act: (cubit) => cubit.doIntent(GetOrdersIntent()),
+      expect: () => [
+        predicate<OrdersState>((state) => state.ordersState!.isLoading),
+
+        predicate<OrdersState>((state) {
+          return state.ordersState!.isLoaded &&
+              state.order?.data?.length == 1 &&
+              state.order?.data?[0].orderId == '1';
+        }),
+      ],
+      verify: (_) {
+        verify(mockUseCase.invoke(1, 10)).called(1);
+      },
+    );
+
+    blocTest<OrdersViewModel, OrdersState>(
+      'emits [loading, error] when GetOrdersIntent fails',
+      build: () {
+        when(
+          mockUseCase.invoke(any, any),
+        ).thenAnswer((_) async => Failure('Server Error'));
+        return viewModel;
+      },
+      act: (cubit) => cubit.doIntent(GetOrdersIntent()),
+      expect: () => [
+        predicate<OrdersState>((state) => state.ordersState!.isLoading),
+        predicate<OrdersState>(
+          (state) =>
+              state.ordersState!.isError &&
+              state.ordersState!.errorMessage == 'Server Error',
+        ),
+      ],
+    );
+
+    blocTest<OrdersViewModel, OrdersState>(
+      'removes order from list and emits RejectOrderEvent when RejectOrderIntent is called',
+      seed: () => OrdersState(
+        ordersState: BaseState.loaded(tOrders),
+        order: BaseState.loaded(tOrders),
+      ),
+      build: () => viewModel,
+      act: (cubit) => cubit.doIntent(RejectOrderIntent(orderId: '1')),
+      expect: () => [
+        predicate<OrdersState>((state) => state.order?.data?.isEmpty ?? false),
+      ],
+    );
+  });
+}
